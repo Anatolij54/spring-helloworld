d97231ff18b5e16688d5f6b5a54eaa303d136998
Anatolij54 <anatolij-chernovol@yandex.ru>
2021-11-03 23:52:32 +0700

    git commit

---
+++ src/main/java/Cat.java
@@ -0,0 +1,22 @@
+public class Cat {
+    private String name;
+    private byte age;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(byte age) {
+        this.age = age;
+    }
+
+    public float getAge() {
+        return age;
+    }
+
+
+}
